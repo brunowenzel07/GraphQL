2fa9e39ed832b04265a779d825bc4936e8e411ba
brunowenzel07 <bruno.wenzel@hotmail.com>
2019-08-28 22:07:39 -0300

    Aula 38

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,9 +1,13 @@
-import { UserModel } from "../models/UserModel";
+import { CommentModel } from "../models/CommentModel";
 import { PostModel } from "../models/PostModel";
+import { UserModel } from "../models/UserModel";
+
+
 
 export interface ModelsInterface{
     
     User: UserModel;
     Post: PostModel;
+    Comment: CommentModel;
 
 }
