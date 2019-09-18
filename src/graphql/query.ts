e7551029dea673b003ea984fcbbd72625a2d53c6
brunowenzel07 <bruno.wenzel@hotmail.com>
2019-09-18 20:29:26 -0300

    aula 50

---
+++ src/graphql/query.ts
@@ -0,0 +1,16 @@
+
+import { postQueries } from './resources/post/post.schema';
+import { userQueries } from './resources/user/user.schema';
+import { commentQueries } from './resources/comment/comment.schema';
+
+const Query =`
+    type Query {
+        ${commentQueries}
+        ${postQueries}
+        ${userQueries}
+    }
+`;
+
+export {
+    Query
+}
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,45 +1,27 @@
 import {makeExecutableSchema} from 'graphql-tools'
-import { ArgumentsOfCorrectType } from 'graphql/validation/rules/ArgumentsOfCorrectType';
-const users: any[] = [
-    {
-        id: 1,
-        name: 'Jon',
-        email:'jon@email.com'
-    },
-    {
-        id: 2,
-        name: 'Dany',
-        email:'dany@email.com'
-    },
-];
 
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!
+import {Query} from './query';
+import {Mutation} from './mutation';
 
-    }
-    type Query{
-        allUsers: [User!]!
-    }   
+import { userTypes } from './resources/user/user.schema';
+import { postTypes } from './resources/post/post.schema';
+import { commentTypes } from './resources/comment/comment.schema';
 
-    type Mutation{
-        createUser(name: String!, email: String!): User
+const SchemaDefinition = `
+
+type Schema {
+        query: Query
+        mutation: Mutation
     }
 `;
 
-const resolvers ={
-    Query: {
-        allUsers: () => users
-    },
-    Mutation: {
-        createUser:(parent, args) =>{
-            const newUser = Object.assign({id: users.length + 1}, args);
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
-
-export default makeExecutableSchema({typeDefs, resolvers});
+export default makeExecutableSchema({
+    typeDefs:[
+        SchemaDefinition,
+        Query,
+        Mutation,
+        commentTypes,
+        postTypes,
+        userTypes
+    ]
+});
